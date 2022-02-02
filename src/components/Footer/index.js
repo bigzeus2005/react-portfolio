420d8b73bd122aaf0fe4d648ef50116ef4389e67
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-02-01 20:12:35 -0500

    updated Footer and Projects

---
+++ src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <div>
+          <a href='mailto:funguy@fun.com'>
+            funguy@fun.com
+          </a>
+        </div>
+        <div>
+          <a href='https://github.com'>
+            GitHub
+          </a>
+        </div>
+        <div>
+          <a href='https://linkedin.com'>
+            LinkedIn
+          </a>
+        </div>
+        <div>
+          <a href='tel:555-555-5555'>
+            555-555-5555
+          </a>
+        </div>
+      </footer>
+    </div>
+  )
+};
+
+export default Footer;
---
+++ src/components/Projects/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      <div>
+        <h3>My Projects</h3>
+      </div>
+      <div>
+        <div>
+          <h2>Project 1</h2>
+          <img src='/assets/images/project1.jpg' alt='project1'></img>
+        </div>
+        <div>
+          <h2>Project 2</h2>
+          <img src='/assets/images/project2.jpg' alt='project2'></img>
+        </div>
+        <div>
+          <h2>Project 3</h2>
+          <img src='/assets/images/project3.jpg' alt='project3'></img>
+        </div>
+        <div>
+          <h2>Project 4</h2>
+          <img src='/assets/images/project4.jpg' alt='project4'></img>
+        </div>
+        <div>
+          <h2>Project 5</h2>
+          <img src='/assets/images/project5.jpg' alt='project5'></img>
+        </div>
+        <div>
+          <h2>Project 6</h2>
+          <img src='/assets/images/project6.jpg' alt='project6'></img>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Projects;
