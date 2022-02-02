3909ad1c7162987d69aaddbae392d55d900f6fe8
The Beefy Mang <jesus.rosa75@yahoo.com>
2022-02-01 19:43:12 -0500

    updated the about index.js

---
+++ src/components/About/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <div>
+        <h3>About Me</h3>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Posuere ac ut consequat semper viverra nam libero. Enim ut tellus elementum sagittis vitae et leo duis ut. Semper quis lectus nulla at volutpat diam ut venenatis tellus. Bibendum ut tristique et egestas quis ipsum. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Viverra maecenas accumsan lacus vel facilisis. Nunc sed id semper risus. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Risus at ultrices mi tempus imperdiet. Ullamcorper dignissim cras tincidunt lobortis. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing.
+        </p>
+        <p>
+          Sit amet nisl suscipit adipiscing bibendum est ultricies. Dignissim diam quis enim lobortis scelerisque fermentum dui. Quisque non tellus orci ac auctor. Mi quis hendrerit dolor magna eget. Nec nam aliquam sem et. Lectus proin nibh nisl condimentum id. Elementum nibh tellus molestie nunc non blandit. Senectus et netus et malesuada fames ac turpis. Egestas diam in arcu cursus euismod quis viverra nibh cras. Ut sem viverra aliquet eget sit. Viverra aliquet eget sit amet tellus cras adipiscing enim. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Convallis convallis tellus id interdum. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Morbi non arcu risus quis varius quam quisque id. Convallis a cras semper auctor neque vitae. Consectetur adipiscing elit ut aliquam. Turpis egestas pretium aenean pharetra magna ac. Tristique risus nec feugiat in fermentum posuere urna nec.
+        </p>
+        <p>
+          Vel turpis nunc eget lorem dolor. Justo eget magna fermentum iaculis eu. Proin fermentum leo vel orci porta non. Tellus in hac habitasse platea dictumst vestibulum rhoncus. Amet dictum sit amet justo donec enim. Blandit libero volutpat sed cras ornare arcu dui vivamus. Neque vitae tempus quam pellentesque. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Cum sociis natoque penatibus et magnis. Ornare massa eget egestas purus viverra. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Suspendisse potenti nullam ac tortor vitae. Adipiscing at in tellus integer feugiat. Porttitor leo a diam sollicitudin tempor. Elit duis tristique sollicitudin nibh sit amet. Ante metus dictum at tempor commodo. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Facilisis volutpat est velit egestas dui id ornare arcu odio. Donec adipiscing tristique risus nec feugiat in fermentum.
+        </p>
+      </div>
+    </div>
+  )
+};
+
+export default About;
